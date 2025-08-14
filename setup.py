c56fee79843b2238c1c57b90eaad20ce016ec1ce
Mayank Goel <themayankgoel28@gmail.com>
2025-08-14 14:18:52 +0530

    rename package to llm_utilities

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("requirements.txt") as f:
     install_requires = f.read().splitlines()
 
 setup(
-    name='llm_utils',
+    name='llm_utilities',
     version='0.1',
     packages=find_packages(),
     install_requires=install_requires,
@@ -12,5 +12,5 @@ setup(
     description='Utilities for working with LLMs',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/mayank31398/llm_utils',
+    url='https://github.com/MayankGoel28/llm_utilities',
 )
